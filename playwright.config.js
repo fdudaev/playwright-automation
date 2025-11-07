080890df8169fdcb1cce91e4843ffa9b9e3f622f
fdudaev <fdudaev@gmail.com>
2025-11-07 09:25:52 -0500

    Xplore code

--- playwright.config.js
+++ playwright.config.js
@@ -7,92 +7,95 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 1,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
     trace: 'retain-on-failure',
-    video: 'on-first-retry',
-    //video: {'mode': 'retain-on-failure', size: { width: 1920, height: 1080} },
+    //video: 'on',
+    video: { 'mode': 'retain-on-failure', size: { width: 1920, height: 1080 } },
     headless: false,
   },
 
   /* Configure projects for major browsers */
   projects: [
 
-      {
-      name: 'chromium',
-      use: { 
-        ...devices['Desktop Chrome'],
-        viewport: {width: 1850, height: 1080} 
-       },
-    },
-  /* 
 
-  {
-      name: 'chromium',
-      use: { 
-        ...devices['Desktop Chrome'],
-        viewport: {width: 1850, height: 1080} 
-       },
-    },
 
     {
-      name: 'firefox',
-      use: { 
-        ...devices['Desktop Firefox'],
-        viewport: {width: 1850, height: 1000} 
-       },
-    },
-
-    {
-      name: 'webkit',
-      use: { 
-        ...devices['Desktop Safari'],
-        viewport: {width: 1850, height: 1000}  
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1850, height: 1080 }
       },
     },
-
-
-     {
-      name: 'Google Chrome',
-      use: { 
-        ...devices['Desktop Chrome'],
-        channel: 'chrome',
-        viewport: {width: 1850, height: 1080} 
-       },
-    },
-
+    /* 
+  
     {
-      name: 'Microsoft Edge',
-      use: { 
-        ...devices['Desktop Chrome'],
-        channel: 'msedge',
-        viewport: {width: 1850, height: 1080} 
-       },
-    },
-
-*/
-
-
- /*
-  Test against mobile
-     {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 17'] },
-    },
+        name: 'chromium',
+        use: { 
+          ...devices['Desktop Chrome'],
+          viewport: {width: 1850, height: 1080} 
+         },
+      },
+  
+      {
+        name: 'firefox',
+        use: { 
+          ...devices['Desktop Firefox'],
+          viewport: {width: 1850, height: 1000} 
+         },
+      },
+  
+      {
+        name: 'webkit',
+        use: { 
+          ...devices['Desktop Safari'],
+          viewport: {width: 1850, height: 1000}  
+        },
+      },
+  
+  
+       {
+        name: 'Google Chrome',
+        use: { 
+          ...devices['Desktop Chrome'],
+          channel: 'chrome',
+          viewport: {width: 1850, height: 1080} 
+         },
+      },
+  
+      {
+        name: 'Microsoft Edge',
+        use: { 
+          ...devices['Desktop Chrome'],
+          channel: 'msedge',
+          viewport: {width: 1850, height: 1080} 
+         },
+      },
+  
   */
+
+
+    /*
+     Test against mobile
+        {
+         name: 'Mobile Chrome',
+         use: { ...devices['Pixel 5'] },
+       },
+   
+           {
+         name: 'Mobile Safari',
+         use: { ...devices['iPhone 15 Pro Max'] },
+         viewport: { width: 430, height: 932 }
+       },
+     */
   ],
 
 });
--- tests/day04/instSigninXplore.spec.js
+++ tests/day04/instSigninXplore.spec.js
@@ -15,12 +15,11 @@ test.describe('Open QA URl', () => {
         let signinButton = page.locator("//span[text()='Sign In']");
         await signinButton.click();
     });
-
-    test.afterEach('Sign out', async ({page}) => {
+     test.afterEach('Sign out', async ({page}) => {
         let instSignOutButton = page.locator("(//a[@class='reg-inst-signout'])[1]");
         await instSignOutButton.click();
         await expect(page.url()).toContain("signout=success");
-    });
+    }); 
 
     test('Search for Ar# and open pdf ', async ({ page }) => {
         let globalSearchbox = await page.locator("(//input[@type='search' and @aria-label='main'])[1]"); 
@@ -36,6 +35,40 @@ test.describe('Open QA URl', () => {
         await page.click(`//a[@class='stats_PDF_${arNumber} u-flex-display-flex']`);
         await expect(page.url()).toContain('/stamp/stamp.jsp');
 
+        await page.waitForTimeout(3000);
+
         await page.goBack();
     });
+    test ("AI search", async ({page}) => {
+
+        await page.click("//button[@id='mainMenu2']");
+        await page.click("//a[@id='menuitem8']");
+        await page.click("//a[@class='stats-WCIA_IEEE_Xplore_AI_Research']");
+        await expect(page.getByText("Search IEEE Content Only")).toBeVisible();
+        let searchIeeContentOnlyRadiobox = page.locator("//input[@id='ieee']");
+        await searchIeeContentOnlyRadiobox.check();
+        expect (await searchIeeContentOnlyRadiobox.isChecked()).toBeTruthy();
+        let AISearchBox = page.locator("//textarea[@id='search']");
+        await AISearchBox.fill("AI search");
+        let aiSearchButton = page.locator("//button[contains(@class,'stats-IEEE_Research_Navigator_Search')]");
+        await aiSearchButton.click();
+        await page.waitForTimeout(3000);
+        await page.click("(//img[@alt='Navigate to Reading Lens'])[1]");
+        // after clicking line 55 withs //b[text()='Term Category']should be visible
+        await expect(page.getByText("Term Category")).toBeVisible();
+        await page.click("(//a[contains(@class,'stats-document-lh-action-downloadPdf_2')])[1]");
+        await expect(page.url()).toContain('/stamp/stamp.jsp');
+        await page.goBack();
+        
+
+
+
+
+
+    });
+
+
 });
+
+/*         let pageTitle = await page.title();
+        console.log(`The Title is ${pageTitle}`); */
